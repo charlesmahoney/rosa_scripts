36fde50bcca0c50e2f8e96e9f731c449574025fb
Your Name <you@example.com>
2023-07-12 14:50:41 -0400

    update download files

--- download_files.sh
+++ download_files.sh
@@ -7,6 +7,8 @@ mkdir $HOME/bin
 cd $HOME/bin
 wget https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/stable/openshift-client-linux.tar.gz
 wget https://mirror.openshift.com/pub/openshift-v4/clients/rosa/latest/rosa-linux.tar.gz
+wget https://github.com/jqlang/jq/releases/download/jq-1.6/jq-linux64
+mv jq-linux64 jq
 tar -xvf rosa-linux.tar.gz
 tar -xvf openshift-client-linux.tar.gz
 cd $HOME
